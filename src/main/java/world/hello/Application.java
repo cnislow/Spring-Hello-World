a9065039a39e56bcf95f8a671e649e96a37c5bb7
Chad Nislow <cjnislow@gmail.com>
2024-02-17 13:26:32 -0500

    - adds dummy http endpoint - package restructure to move Application into java/world/hello package

--- src/main/java/world/hello/Application.java
+++ src/main/java/world/hello/Application.java
@@ -1,9 +1,11 @@
-package world.hello.spring;
+package world.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "world.hello")
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/world/hello/security/SecurityConfig.java
+++ src/main/java/world/hello/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package security;
+package world.hello.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
